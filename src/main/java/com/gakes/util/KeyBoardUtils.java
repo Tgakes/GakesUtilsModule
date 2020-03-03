bb90e86431436f00666aa27ffe9dedae83661349
Tgakes <gakes@qq.com>
2020-03-03 09:01:21 +0800

    判断键盘是否弹起

--- src/main/java/com/gakes/util/KeyBoardUtils.java
+++ src/main/java/com/gakes/util/KeyBoardUtils.java
@@ -2,6 +2,7 @@ package com.gakes.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Rect;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -52,5 +53,20 @@ public class KeyBoardUtils {
     public static boolean isShowForkeyboard(Context context) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         return imm.isActive();
+    }
+
+    public static boolean isSoftKeyBoardVisible(Activity activity){
+        final View decorView = activity.getWindow().getDecorView();
+        Rect rect = new Rect();
+        decorView.getWindowVisibleDisplayFrame(rect);
+        //计算出可见屏幕的高度
+        int displayHeight = rect.bottom - rect.top;
+        //获得屏幕整体的高度
+        int height = decorView.getHeight();
+        //获得键盘高度
+//        int keyboardHeight = height - displayHeight;
+        boolean visible = (double) displayHeight / height < 0.8;
+
+        return  visible;
     }
 }
